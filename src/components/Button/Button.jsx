a733925ccd51332cd7a06998a594440df5589879
AGorbovyi <anton.gorbovyi@gmail.com>
2024-09-19 00:39:31 +0200

    homework 03, login form final version

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,11 +1,5 @@
 import './styles.css'
 
-// function Button() {
-//     return <button className='button-component'>Login</button>
-// }
-
-// export default Button;
-
 function Button({type = "button", name}) {
     return (
         <button className='button-component' type={type}>

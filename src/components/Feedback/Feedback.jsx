f61c4e0c9950ac5f9d1f36ae8812431037d034cd
AGorbovyi <anton.gorbovyi@gmail.com>
2024-09-20 19:56:33 +0200

    homework 04. Final version

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -5,47 +5,6 @@ import './styles.css';
 
 function Feedback () {
 
-    // const [count, setCount] = useState(0);
-    // const [hover, setHover] = useState(false);
-
-    // let className = 'counter';
-    // if (hover) {
-    //   className += ' hover';
-    // }
-    // return (
-    //     <div
-    //       className={className}
-    //       onPointerEnter={() => setHover(true)}
-    //       onPointerLeave={() => setHover(false)}
-    //     >
-    //       <h1>{score}</h1>
-    //       <button onClick={() => setScore(score + 1)}>
-    //         Add one
-    //       </button>
-    //     </div>
-    //   );
-    // const [isPaused, setIsPaused] = useState(false);
-    // return (
-    //   <div>
-    //     {isPaused ? (
-    //       <p>See you later!</p> 
-    //     ) : (
-    //       <Counter /> 
-    //     )}
-    //     <label>
-    //       <input
-    //         type="checkbox"
-    //         checked={isPaused}
-    //         onChange={e => {
-    //           setIsPaused(e.target.checked)
-    //         }}
-    //       />
-    //       Take a break
-    //     </label>
-    //   </div>
-    // );
-
-
     const [likes, setLike] = useState(0);
     const like = () => {
         setLike((likes) => {
@@ -60,19 +19,10 @@ function Feedback () {
         });
     }
 
-    // const [resetValue, setReset] = useState(0);
-    // const reset = () => {
-    //     setReset((previousValue) => {
-    //         return ++previousValue;
-    //     });
-    // }
-    const [resetValue, setReset] = useState(false);
     const reset = () => {
-        setReset(() => {
-            return null;
-        });
+        setLike(0);
+        setDislike(0);
         }
-
     
     return (
         <div className='feedback-form-wrapper'>
